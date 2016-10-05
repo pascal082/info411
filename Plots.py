4f727a830a16ceabeee3a7b46948324f2f682dd6
raybalta <raymundbaltazar@yahoo.com>
2016-10-05 19:29:06 +1300

    addded plot and fixed SVM

--- Plots.py
+++ Plots.py
@@ -5,6 +5,7 @@ from matplotlib.pylab import rcParams
 import datetime
 from datetime import timedelta
 
+
 rcParams['figure.figsize'] = 15, 6
 
 ## load data set
@@ -61,14 +62,63 @@ for i in merge_data['month']:
 
 merge_data['season'] = season
 
-#print merge_data.head(4)
+
 #merge_data.tail()
 
+rollingAve = pd.rolling_mean(merge_data['nswprice'], 48)
+##merge_data.append(rollingAve);
+
+#print merge_data.head(4)
+##plot all demand and price
+
+#print rollingAve
+
+
+## rolling average
+plt.plot(merge_data['new_date'], merge_data['nswprice'],'blue', label="NSW Price")
+plt.plot(merge_data['new_date'], rollingAve,'red', label="NSW Demand")
+plt.plot(merge_data['new_date'], merge_data['nswdemand'], 'green', label =" NSW Demand")
+#plt.show()
+
+#month
+#plt.plot(merge_data['new_date'], merge_data['nswprice'],'blue', label="NSW Price")
+#plt.plot(merge_data['new_date'], merge_data['nswdemand'],'red', label="NSW Demand")
+#plt.show()
+
+##summer
 summer = merge_data[merge_data.season =='summer']
+winter = merge_data[merge_data.season =='winter']
 
-#print summer.head()
-#print summer.tail()
+print len (winter)
 
-plt.plot(summer['new_date'],summer['nswprice'],'blue',label="NSW Summer Price")
-plt.plot(summer['new_date'],summer['nswdemand'],'red',label="NSW Summer Demand")
-plt.show()
+winter = winter.drop(winter.index[0:],)
+print len (winter)
+print merge_data.iloc[[4],]
+
+summer= summer.append(winter, ignore_index = True)
+#print "head:" ,summer.head()
+#print  "tail:" ,summer.tail()
+
+s1 = summer[summer.season == 'winter']
+
+#print s1.head()
+#plt.plot(summer['new_date'],summer['nswprice'],'blue',label="NSW Summer Price")
+#plt.plot(summer['new_date'],summer['nswdemand'],'red',label="NSW Summer Demand")
+#plt.show()
+
+## plot daily
+week1 = merge_data[0:337]
+
+#print week1.head()
+
+#plt.plot(week1['new_date'],week1['nswprice'],'blue',label="NSW Summer Price")
+#plt.plot(week1['new_date'],week1['nswdemand'],'red',label="NSW Summer Demand")
+#plt.show()
+
+
+yr1998 = merge_data[merge_data.year == 1998]
+
+month198 = yr1998[yr1998.month == 9]
+#plt.plot(month198['new_date'],month198['nswprice'],'blue',label="NSW May 98 Price")
+#plt.plot(month198['new_date'],month198['nswdemand'],'red',label="NSW May 98 Demand")
+#plt.show()
